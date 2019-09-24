c15af0cd50c8ff1317e28cfccef1ea0c78d51d5f
zrphercule <zrphercule@gmail.com>
2019-09-24 15:32:06 -0700

    Fix PYTORCH CI break (#3544)
    Summary:
    Last week we have some API update in pytorch quantization jit ir, which caused our CI down.
    This pr fixed this problem, and all tests should be passed.
    Pull Request resolved: https://github.com/pytorch/glow/pull/3544
    
    Differential Revision: D17537650
    
    Pulled By: zrphercule
    
    fbshipit-source-id: 85a7f1ba48b7040085f1b5649e084f70547827da

--- torch_glow/tests/nodes/quantized_avgpool2d_test.py
+++ torch_glow/tests/nodes/quantized_avgpool2d_test.py
@@ -5,17 +5,17 @@ import torch
 from tests.utils import jitVsGlow
 
 
-def test_quantized_relu():
-    """Basic test of the PyTorch quantized::relu Node on Glow."""
+def test_quantized_avgpool():
+    """Basic test of the PyTorch quantized::avg_pool2d Node on Glow."""
 
     def test_f(a):
         q = torch.nn.quantized.Quantize(1/128, 3, torch.quint8)
         dq = torch.nn.quantized.DeQuantize()
-        re = torch.nn.quantized.ReLU()
-        return dq(re(q(a)))
+        ap = torch.nn.AvgPool2d(3)
+        return dq(ap(q(a)))
 
-    x = torch.randn([5, 5])
+    inputs = torch.randn(1, 4, 5, 5)
 
-    jitVsGlow(test_f, x, expected_fused_ops={"aten::relu",
+    jitVsGlow(test_f, inputs, expected_fused_ops={"aten::avg_pool2d",
                                              "aten::quantize_per_tensor",
                                              "aten::dequantize"})
--- torch_glow/tests/nodes/quantized_maxpool2d_test.py
+++ torch_glow/tests/nodes/quantized_maxpool2d_test.py
@@ -5,17 +5,17 @@ import torch
 from tests.utils import jitVsGlow
 
 
-def test_quantized_relu():
-    """Basic test of the PyTorch quantized::relu Node on Glow."""
+def test_quantized_maxpool():
+    """Basic test of the PyTorch quantized::max_pool2d Node on Glow."""
 
     def test_f(a):
         q = torch.nn.quantized.Quantize(1/128, 3, torch.quint8)
         dq = torch.nn.quantized.DeQuantize()
-        re = torch.nn.quantized.ReLU()
-        return dq(re(q(a)))
+        mp = torch.nn.MaxPool2d(3)
+        return dq(mp(q(a)))
 
-    x = torch.randn([5, 5])
+    inputs = torch.randn(1, 4, 5, 5)
 
-    jitVsGlow(test_f, x, expected_fused_ops={"aten::relu",
+    jitVsGlow(test_f, inputs, expected_fused_ops={"aten::max_pool2d",
                                              "aten::quantize_per_tensor",
                                              "aten::dequantize"})
